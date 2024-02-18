48201ae1d5bfd82f26ac83fc935fb6234d8ccb5d
Pedro Jose Soto Rivera <pedroj.sotor@uqvirtual.edu.co>
2024-02-18 13:46:43 -0500

    suma de la clase

---
+++ universidad/src/main/java/co/edu/uniquindio/poo/Estudiante.java
@@ -0,0 +1,10 @@
+package co.edu.uniquindio.poo;
+
+public record Estudiante(String nombres,String apellidos,String identificacion,String telefono,String correo,byte edad) {
+
+    public Object identificacion() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'identificacion'");
+    }
+
+}
